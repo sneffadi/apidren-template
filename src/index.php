5028da632b614226d6b6edb949cff0af669da2e5
Stephanie <sneffadi@gmail.com>
2015-10-26 12:05:56 -0600

    added buytable template and testimonials

--- src/index.php
+++ src/index.php
@@ -12,25 +12,46 @@ get_header(); ?>
 
 
 ?>
-
-
         <div id="testimonials">
-            <div class="testomonial">
-
-            </div> <!-- / .testomonial -->
-            <div class="testomonial">
-
-            </div> <!-- / .testomonial -->
-            <div class="testomonial">
-
-            </div> <!-- / .testomonial -->
-            <div class="testomonial">
-
-            </div> <!-- / .testomonial -->
-            <div class="testomonial">
-
-            </div> <!-- / .testomonial -->
+            <h2>Testimonials</h2>
+            <div class="row testimonial">
+                <div class="small-12 large-4 columns">
+                    <img class="test">
+                </div>
+                <div class="small-12 large-8 columns">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam in cursus arcu. Aenean cursus faucibus eleifend. Quisque a felis scelerisque, egestas nulla sed, venenatis ligula. Aliquam arcu justo, faucibus et hendrerit ut, lacinia in nunc. Aenean non ligula eu dui feugiat lobortis. Fusce mattis urna quis dui tristique, tristique pellentesque nunc hendrerit. Proin consectetur eros tellus, eget blandit urna pharetra maximus.</p>
+                    <p class="name">- John Doe</p>
+                </div>
+            </div>
+            <div class="row testimonial">
+                <div class="small-12 large-4 columns">
+                    <img class="test">
+                </div>
+                <div class="small-12 large-8 columns">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam in cursus arcu. Aenean cursus faucibus eleifend. Quisque a felis scelerisque, egestas nulla sed, venenatis ligula. Aliquam arcu justo, faucibus et hendrerit ut, lacinia in nunc. Aenean non ligula eu dui feugiat lobortis. Fusce mattis urna quis dui tristique, tristique pellentesque nunc hendrerit. Proin consectetur eros tellus, eget blandit urna pharetra maximus.</p>
+                    <p class="name">- John Doe</p>
+                </div>
+            </div>
+            <div class="row testimonial">
+                <div class="small-12 large-4 columns">
+                    <img class="test">
+                </div>
+                <div class="small-12 large-8 columns">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam in cursus arcu. Aenean cursus faucibus eleifend. Quisque a felis scelerisque, egestas nulla sed, venenatis ligula. Aliquam arcu justo, faucibus et hendrerit ut, lacinia in nunc. Aenean non ligula eu dui feugiat lobortis. Fusce mattis urna quis dui tristique, tristique pellentesque nunc hendrerit. Proin consectetur eros tellus, eget blandit urna pharetra maximus.</p>
+                    <p class="name">- John Doe</p>
+                </div>
+            </div>
+            <div class="row testimonial">
+                <div class="small-12 large-4 columns">
+                    <img class="test">
+                </div>
+                <div class="small-12 large-8 columns">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam in cursus arcu. Aenean cursus faucibus eleifend. Quisque a felis scelerisque, egestas nulla sed, venenatis ligula. Aliquam arcu justo, faucibus et hendrerit ut, lacinia in nunc. Aenean non ligula eu dui feugiat lobortis. Fusce mattis urna quis dui tristique, tristique pellentesque nunc hendrerit. Proin consectetur eros tellus, eget blandit urna pharetra maximus.</p>
+                    <p class="name">- John Doe</p>
+                </div>
+            </div>
         </div> <!-- /#testimonials  -->
+
         <div id="faq" class="expandable">
             <h3>Frequently Asked Questions  <span class="show-hide-all" data-text-swap="Hide All [-]" data-text-original="Show All [+]"></span></h3>
 
@@ -121,6 +142,134 @@ get_header(); ?>
                 </div> <!-- / .columns -->
             </div> <!-- / .row -->
         </div> <!-- /#faq  -->
+        <div class="row content">
+            <h2>Order <?php echo $productName ?> Today!</h2>
+            <div class="large-12 columns large-centered">
+            <div class="row buytable" id="buy">
+            <div class="small-12 large-4 columns buy buy1 radius">
+                <div class="row buy-head">
+                    Get Started
+                </div>                    
+                <form action="<?php echo $formURL ?>" method="get">
+                    <button class="products">
+                        <img src="assets/images/buy1.png">
+                    </button>
+                    <div class="row priceWrap">
+                        <div class="small-6 columns">
+                            Retail: $99.95
+                        </div>
+                        <div class="small-6 columns savings">
+                            $50 Savings
+                        </div>
+                    </div> <!-- / .row -->
+                    <div class="row price">
+                        <sup>$</sup>49<sup>95</sup>
+                    </div>
+                    <div class="row bottle">
+                        1 Bottle - 30 Day Supply
+                    </div>
+                    <div class="row bonusWrap">
+                        <div class="buy-bonus bonus1">
+                            $4.95 Flat-Rate Shipping
+                        </div>
+                    </div>
+                    <div class="row">
+                        <button class="addtoCart radius">
+                            Buy Now
+                        </button>
+                    </div>
+                    <input name="add" type="hidden" value="apidren1">
+                </form>
+            </div><!-- /.buy1 -->
+            
+            <div class="small-12 large-4 columns buy buy2">
+                <div class="row buy-head mostPopular">
+                    Most Popular
+                </div>
+                <form action="<?php echo $formURL ?>" method="get">
+                    <button class="products">
+                        <img src="assets/images/buy3.png">
+                    </button>
+                    <div class="row priceWrap">
+                        <div class="small-6 columns">
+                            Retail: $79.95
+                        </div>
+                        <div class="small-6 columns savings">
+                            $120 Savings
+                        </div>
+                    </div> <!-- / .row -->
+                    <div class="row price">
+                        <sup>$</sup>79<sup>95</sup>
+                    </div>
+                    <div class="row bottle">
+                        2 Bottles - 60 Day Supply
+                    </div>
+                    <div class="row bonusWrap">
+                        <div class="buy-bonus bonus1">
+                            Free Shipping
+                        </div>
+                        <div class="buy-bonus bonus2">
+                            +1 Free Bottle of 72HC
+                        </div>
+                    </div>
+                    <div class="row">
+                        <button class="addtoCart radius">
+                            Buy Now
+                        </button>
+                    </div>
+                    <input name="add" type="hidden" value="apidren3">
+                </form>
+            </div><!-- /.buy2 -->
+            <div class="small-12 large-4 columns buy buy3">
+                <div class="row buy-head">
+                    Best Value
+                </div> 
+                <form action="<?php echo $formURL ?>" method="get">
+                    <button class="products">
+                        <img src="assets/images/buy2.png">
+                    </button>
+                    <div class="row priceWrap">
+                        <div class="small-6 columns">
+                            Retail: $299.85
+                        </div>
+                        <div class="small-6 columns savings">
+                            $190 Savings
+                        </div>
+                    </div> <!-- / .row -->
+                    <div class="row price">
+                        <sup>$</sup>109<sup>95</sup>
+                    </div>
+                    <div class="row bottle">
+                        3 Bottles - 90 Day Supply
+                    </div>
+                    <div class="row bonusWrap">
+                        <div class="buy-bonus bonus1">
+                            Free Shipping
+                        </div>
+                        <div class="buy-bonus bonus2">
+                            +3 Free Bottles of 72HC
+                        </div>
+                    </div>
+                    <div class="row">
+                        <button class="addtoCart radius">
+                            Buy Now
+                        </button>
+                    </div>
+                    <input name="add" type="hidden" value="apidren3">
+                </form>
+            </div><!-- /.buy3 -->   
+        </div><!-- /.row.buytable -->
+        <div class="row">
+            <div class="payment">   
+                <i class="fa fa-cc-visa"></i>
+                <i class="fa fa-cc-amex"></i>
+                <i class="fa fa-cc-mastercard"></i>
+                <i class="fa fa-cc-discover"></i>
+                <i class="fa fa-cc-paypal"></i>
+            </div> <!-- / .payment -->
+        </div>
+    </div> <!-- / .large-10 -->
+</div><!-- / .row.cotent -->
     </div> <!-- / .columns -->
 </div> <!-- / .row -->
 <?php
