290adab0ed198bfa7b3457c9d28b00bddfec623d
Stephanie <sneffadi@gmail.com>
2015-10-27 10:25:09 -0600

    front pop tweaks

--- src/assets/javascript/custom/main.js
+++ src/assets/javascript/custom/main.js
@@ -1,27 +1,15 @@
 //https://github.com/flesler/jquery.scrollTo
 // sets defaults
-// jQuery(document).ready(function($) {
-//     $.extend($.scrollTo.defaults, {
-//         axis: 'y',
-//         duration: 700,
-//         easing: 'swing',
-//         margin: true,
-//     });
-//     $(".go-to-buy-section").on("click", function() {
-//         $.scrollTo("#buy-section");
-//     });
-// });
-$(function() {
-        $('a[href*=#]:not([href=#])').click(function() {
-            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-                var target = $(this.hash);
-                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-                if (target.length) {
-                    $('html,body').animate({
-                        scrollTop: target.offset().top
-                    }, 1000);
-                    return false;
-                }
-            }
-        });
-    }); /* ! https://github.com/robflaherty/jquery-scrolldepth | v0.4.1 Copyright (c) 2014 Rob Flaherty (@robflaherty) Licensed under the MIT and GPL licenses. */ ;
+jQuery(document).ready(function($) {
+    $.extend($.scrollTo.defaults, {
+        axis: 'y',
+        duration: 700,
+        easing: 'swing',
+        margin: true,
+    });
+    $(".go-to-buy-section").on("click", function() {
+        $.scrollTo("#buy-section");
+    });
+});
+
+jQuery(function() { _exitPop.init("parts/frontpop.html"); });
