2287ae2a2bce34e510d68b87e70d16df806f86fa
Brett DeVore <brett@unkubed.com>
2015-11-03 17:45:33 -0700

    First commit

--- src/assets/javascript/custom/faq-expandable.js
+++ src/assets/javascript/custom/faq-expandable.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function($) {
     $("#faq.expandable").find(".q").append("<span class=\"show-hide\"></span>");
-    $("#faq.expandable").find(".show-hide").data("text-swap", '<i class="fa fa-minus-square-o"></i>').data("text-original", '<i class="fa fa-plus-square-o"></i>');
+    $("#faq.expandable").find(".show-hide").data("text-swap", '<i class="fa fa-minus"></i>').data("text-original", '<i class="fa fa-plus"></i>');
     var original = $("#faq.expandable").find(".show-hide").data("text-original"),
         swap = $("#faq.expandable").find(".show-hide").data("text-swap");
     $("#faq.expandable").find(".show-hide-all").html($("#faq").find(".show-hide-all").data("text-original"));
