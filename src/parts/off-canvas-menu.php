1b132e80cefd847930f80be5f63f55bafbf76cd8
Stephanie <sneffadi@gmail.com>
2015-11-25 11:50:22 -0700

    first real finish

--- src/parts/off-canvas-menu.php
+++ src/parts/off-canvas-menu.php
@@ -8,25 +8,12 @@
 </nav>
 <aside class="right-off-canvas-menu">
     <ul class="off-canvas-list">
-        <li><label>Level 1</label></li>
-        <li><a href="#">Level 1, Link 1</a></li>
-        <li class="has-submenu"><a href="#">Level 1, Link 2</a>
-            <ul class="right-submenu">
-                <li class="back"><a href="#">Back</a></li>
-                <li><label>Level 2</label></li>
-                <li class="has-submenu"><a href="#">Level 2, Link 1 w/ submenu</a>
-                    <ul class="right-submenu">
-                        <li class="back"><a href="#">Back</a></li>
-                        <li><label>Level 3</label></li>
-                        <li><a href="#">Level 3, Link 1</a></li>
-                        <li><a href="#">Level 3, Link 2</a></li>
-                        <li><a href="#">Level 3, Link 3</a></li>
-                    </ul>
-                </li>
-                <li><a href="#">Level 2, Link 2</a></li>
-                <li><a href="#">Level 2, Link 3</a></li>
-            </ul>
-        </li>
-        <li><a href="#">Level 1, Link 3</a></li>
+        <li><label>Menu</label></li>
+        <li><a href="#">Home</a></li>
+        <li><a href="parts/guarantee.php" data-reveal-ajax="true" data-reveal-id="support" >90-Day Guarantee</a></li>
+        <li><a href="#faq">FAQ</a></li>
+        <li><a href="#ingredients">Ingredients</a></li>
+        <li><a href="parts/contact.php" data-reveal-ajax="true" data-reveal-id="support" >Contact us</a></li>
+        <li><a href="#buy" class="go-to-buy-section"><b>Buy Apidren</b></a></li>
     </ul>
 </aside>
